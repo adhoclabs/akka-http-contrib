17436df81af6f52a892b851b426342cf265f3111
cppexpert <github@walkmind.com>
2016-09-18 19:10:40 +0300

    Reject request that exceeded limit with `TooManyRequestsRejection` rejection instead of responding with complete directive.

--- src/main/scala/co/adhoclabs/akka/http/contrib/throttle/ThrottleDirective.scala
+++ src/main/scala/co/adhoclabs/akka/http/contrib/throttle/ThrottleDirective.scala
@@ -1,18 +1,17 @@
 package co.adhoclabs.akka.http.contrib.throttle
 
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.directives.{ BasicDirectives, FutureDirectives, RouteDirectives }
-import akka.http.scaladsl.server.{ Directive, Directive0 }
+import akka.http.scaladsl.server.directives.{BasicDirectives, FutureDirectives, RouteDirectives}
+import akka.http.scaladsl.server.{Directive, Directive0}
 
 trait ThrottleDirective {
   import BasicDirectives._
-  import RouteDirectives._
   import FutureDirectives._
+  import RouteDirectives._
 
   def throttle(implicit settings: ThrottleSettings): Directive0 = extractRequest.flatMap { r ⇒
     onSuccess(settings.shouldThrottle(r)).flatMap { should ⇒
       if (should) {
-        complete(StatusCodes.TooManyRequests)
+        reject(TooManyRequestsRejection())
       } else {
         settings.onExecute(r)
         Directive.Empty
